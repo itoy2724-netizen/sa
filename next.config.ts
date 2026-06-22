a4584f1fe95c6d2abbe0316211a1efdf1407f6d6
itoy2724-netizen <itoy2724@gmail.com>
2026-06-23 02:48:33 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+     {
+  serverExternalPackages: ['mysql2'],
+};
+
+
     images: {
         remotePatterns: [
             {
